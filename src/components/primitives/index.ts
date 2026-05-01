0bbefdd501c8fe639b5c821427203955c1b0e6ca
Nick Guizol <nickguizol14@gmail.com>
2026-05-01 12:16:56 -0500

    chore(primitives): add barrel export + Plan 1 verification

---
+++ src/components/primitives/index.ts
@@ -0,0 +1,21 @@
+// src/components/primitives/index.ts
+export { GoldRule } from "./GoldRule";
+export type { GoldRuleProps } from "./GoldRule";
+
+export { DriftParticles } from "./DriftParticles";
+export type { DriftParticlesProps } from "./DriftParticles";
+
+export { BurstParticles } from "./BurstParticles";
+export type { BurstParticlesProps, BurstParticlesHandle } from "./BurstParticles";
+
+export { TypeParticles } from "./TypeParticles";
+export type { TypeParticlesProps, TypeParticlesHandle } from "./TypeParticles";
+
+export { ChapterDivider } from "./ChapterDivider";
+export type { ChapterDividerProps } from "./ChapterDivider";
+
+export { ChapterBreak } from "./ChapterBreak";
+export type { ChapterBreakProps } from "./ChapterBreak";
+
+export { BluePanel } from "./BluePanel";
+export type { BluePanelProps } from "./BluePanel";
